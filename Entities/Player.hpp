63e6a8ab87daba7255d98105d73a5030896d7866
brit45 <36789485+brit45@users.noreply.github.com>
2022-09-29 17:53:21 +0200

    Create player class

---
+++ Entities/Player.hpp
@@ -0,0 +1,14 @@
+#ifndef __ENTITIES_PLAYER_HPP__
+#define __ENTITIES_PLAYER_HPP__
+
+#include "Entity.hpp"
+
+class Player : public Entity {
+
+    public:
+
+        Player();
+        virtual ~Player();
+}
+
+#endif //! __ENTITIES_PLAYER_HPP__
